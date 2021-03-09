4dfb0b55ffa0fb5056ab2d5021c05a3995b777dc
jmjp775 <jmjp775@gmail.com>
2021-03-09 15:29:46 -0500

    first commit

---
+++ DisplayDateTime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateTimeExamples
+{
+    public class DisplayDateTime
+    {
+        public static string datePatt = @"M/d/yyy hh:mm:ss tt";
+        public static void Display(string title, DateTime inputDt)
+        {
+            DateTime dispDt = inputDt;
+            string dtString;
+
+            //Display the original DateTime.
+
+            dtString = dispDt.ToString(datePatt);
+            Console.WriteLine("{0} {1}, Kind = {2}",
+                                title, dtString, dispDt.Kind);
+
+            //Convert inputDt to local time and disply the result.
+            //If inputDt.Kind is DateTimeKind.Utc, the conversion is performed.
+            //If inputDt.Kind is DateTimeKind.Local, the conversion is not performed.
+            //If inputDt.Kind is DateTimeKind.Unspecified, the conversion is
+            //performed as if inputDt was universal time.
+            dispDt = inputDt.ToUniversalTime();
+            dtString = dispDt.ToString(datePatt);
+            Console.WriteLine(" ToUniversalTime: {0}, Kind = {1}",
+                              dtString, dispDt.Kind);
+            Console.WriteLine();
+        }
+
+        public static void DisplayNow(string title, DateTime inputDt)
+        {
+            string dtString = inputDt.ToString(datePatt);
+            Console.WriteLine("{0} {1}, Kind = {2}",
+                               title, dtString, inputDt.Kind);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DateTimeExamples
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Get the date and time for the current moment, 
+            //adjusted to the local time zone.
+            DateTime saveNow = DateTime.Now;
+
+            //Get the date and time for the current moment expressed
+            //as coordinated universal time (UTC).
+            DateTime saveUtcNow = DateTime.UtcNow;
+            DateTime myDt;
+
+            //Display the value and Kind property of the current moment
+            //expressed as UTC and local time.
+            DisplayDateTime.DisplayNow("UtcNow: ..........", saveUtcNow);
+            DisplayDateTime.DisplayNow("Now: .............", saveNow);
+            Console.WriteLine();
+
+            //Change the Kind property of the current moment to
+            //DateTimeKind.Utc and display the result.
+            myDt = DateTime.SpecifyKind(saveNow, DateTimeKind.Utc);
+            DisplayDateTime.Display("Utc: .............", myDt);
+
+            //Change the Kind property of the current moment to
+            //DateTimeKind.Local and display the result.
+            myDt = DateTime.SpecifyKind(saveNow, DateTimeKind.Local);
+            DisplayDateTime.Display("Local: ...........", myDt);
+
+            //Change the Kind property of the current moment to
+            //DateTimeKind.Unspecified and display the result.
+            myDt = DateTime.SpecifyKind(saveNow, DateTimeKind.Unspecified);
+            DisplayDateTime.Display("Unspecified: .....", myDt);
+        }
+    }
+}
